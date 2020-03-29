c41435c86ac683322a2c69aa4bdccdff2e548b58
Yang Chen <35882376+cystanford@users.noreply.github.com>
2020-03-29 23:23:01 +0800

    Add files via upload

---
+++ L3/wordcloud/movies_word_cloud.py
@@ -0,0 +1,45 @@
+# -*- coding:utf-8 -*-
+# 词云展示
+from wordcloud import WordCloud
+import pandas as pd
+import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
+from lxml import etree
+from nltk.tokenize import word_tokenize
+
+# 去掉停用词
+def remove_stop_words(f):
+	stop_words = ['Movie']
+	for stop_word in stop_words:
+		f = f.replace(stop_word, '')
+	return f
+
+# 生成词云
+def create_word_cloud(f):
+	print('根据词频，开始生成词云!')
+	f = remove_stop_words(f)
+	cut_text = word_tokenize(f)
+	#print(cut_text)
+	cut_text = " ".join(cut_text)
+	wc = WordCloud(
+		max_words=100,
+		width=2000,
+		height=1200,
+    )
+	wordcloud = wc.generate(cut_text)
+	# 写词云图片
+	wordcloud.to_file("wordcloud.jpg")
+	# 显示词云文件
+	plt.imshow(wordcloud)
+	plt.axis("off")
+	plt.show()
+
+# 数据加载
+data = pd.read_csv('./movies.csv')
+# 读取title 和 genres字段
+title = " ".join(data['title'])
+genres = " ".join(data['genres'])
+all_word = title + genres
+# 生成词云
+create_word_cloud(text)
